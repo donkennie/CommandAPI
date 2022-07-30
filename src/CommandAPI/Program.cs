0a5dedc0b06ec4ef45951f630212c2c2aceb1b26
donkennie <ajeigbekehinde160@gmail.com>
2022-07-30 15:38:17 +0100

    add data context

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,5 +1,16 @@
+using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
