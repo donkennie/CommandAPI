871af823a9eeec9462c5d1c73101f2267090c476
donkennie <ajeigbekehinde160@gmail.com>
2022-07-30 16:17:50 +0100

    add migration

---
+++ src/CommandAPI/Migrations/20220730151250_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CommandAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CommandItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HowTo = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Platform = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommandLine = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommandItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommandItems");
+        }
+    }
+}
