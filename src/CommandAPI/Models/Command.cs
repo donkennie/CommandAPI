a7caa50b0ebd2c946bdbe88772a3338095867e30
donkennie <ajeigbekehinde160@gmail.com>
2022-07-30 15:03:07 +0100

    Added Command Model to API Project

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandAPI.Models
+{
+    public class Command
+    {
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
