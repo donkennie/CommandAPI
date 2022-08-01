c6e6bf36c23f8e090cc6a1f06fdeed28862658cf
donkennie <ajeigbekehinde160@gmail.com>
2022-08-02 00:47:53 +0100

    Test class model

---
+++ test/CommandAPI.Tests/CommandTest.cs
@@ -0,0 +1,67 @@
+using CommandAPI.Models;
+
+namespace CommandAPI.Tests;
+
+public class CommandTest : IDisposable
+{
+
+        Command testCommand;
+
+     public CommandTest()
+     {
+        testCommand = new Command
+        {
+            HowTo = "Do something",
+            Platform = "Some platform",
+            CommandLine = "Some commandline"
+        };
+     }
+
+      public void Dispose()
+     {
+        testCommand = null;
+     }
+
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Arrange
+
+            //Act
+            testCommand.HowTo = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+            //Arrange
+
+            //Act
+            testCommand.Platform = "xUnit";
+
+            //Assert
+            Assert.Equal("xUnit", testCommand.Platform);
+
+        }
+
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            //Arrange
+
+            //Act
+            testCommand.CommandLine = "dotnet test";
+
+            //Assert
+            Assert.Equal("dotnet test", testCommand.CommandLine);
+
+        }
+
+    
+
+}
